e5c374383e556bca91fac2ddeb8fac960baac579
Adolfo García Veytia (Puerco) <puerco@carabiner.dev>
2025-05-08 19:45:18 -0600

    GroupGet unit test
    Signed-off-by: Adolfo García Veytia (Puerco) <puerco@carabiner.dev>

--- locator_test.go
+++ locator_test.go
@@ -4,6 +4,8 @@
 package vcslocator
 
 import (
+	"crypto/sha256"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -75,6 +77,94 @@ func TestParseLocator(t *testing.T) {
 			require.Equal(t, tc.expect.Commit, res.Commit, "Commit mismatch")
 			require.Equal(t, tc.expect.Branch, res.Branch, "Branch mismatch")
 			require.Equal(t, tc.expect.Tag, res.Tag, "Tag mismatch")
+		})
+	}
+}
+
+func TestGetGroup(t *testing.T) {
+	t.Parallel()
+	for _, tc := range []struct {
+		name     string
+		locators []string
+		expect   []string
+		mustErr  bool
+	}{
+		{
+			"single",
+			[]string{"git+https://github.com/carabiner-dev/vcslocator@76241a877eb3374f6017224c61d6a167c337de4d#.gitignore"},
+			[]string{"b319f85e4a246c38474a242ecaee46ca514c4abcfae781f0f7e2a7a58b3e5a4f"},
+			false,
+		},
+		{
+			"two",
+			[]string{
+				"git+https://github.com/carabiner-dev/vcslocator@b145fcf66fe321522ca093de00646f8c1e482e8d#components.go",
+				"git+https://github.com/carabiner-dev/vcslocator@cb1adf0eb1179e26228091c3a347d037ae7b4460#components.go",
+			},
+			[]string{
+				"58c76f62c2d403aa2d946f53b381f4948f12a6814482d50fb4fd3d87f45e38d3",
+				"58c76f62c2d403aa2d946f53b381f4948f12a6814482d50fb4fd3d87f45e38d3",
+			},
+			false,
+		},
+		{
+			"two-and-two-repos",
+			[]string{
+				"git+https://github.com/carabiner-dev/vcslocator@b145fcf66fe321522ca093de00646f8c1e482e8d#components.go",
+				"git+https://github.com/carabiner-dev/vcslocator@cb1adf0eb1179e26228091c3a347d037ae7b4460#components.go",
+				"git+https://github.com/carabiner-dev/actions@ecdd8b03b5c1bad78d5d89ab71e1ca9bb5ad31c9#drop/action.yml",
+				"git+https://github.com/carabiner-dev/actions@ecdd8b03b5c1bad78d5d89ab71e1ca9bb5ad31c9#install/ampel/action.yml",
+			},
+			[]string{
+				"58c76f62c2d403aa2d946f53b381f4948f12a6814482d50fb4fd3d87f45e38d3",
+				"58c76f62c2d403aa2d946f53b381f4948f12a6814482d50fb4fd3d87f45e38d3",
+				"abf988eca60b353c1a1a030219466acc8d355c35a1e40c508e392dd3496be525",
+				"e8d84a48c5240adeb41ba5d66fce91cc6df2ef87031debacdd8ab56f40c2227b",
+			},
+			false,
+		},
+		{
+			"takes-six-to-tango-the-throttler",
+			[]string{
+				"git+https://github.com/carabiner-dev/vcslocator@b145fcf66fe321522ca093de00646f8c1e482e8d#components.go",
+				"git+https://github.com/carabiner-dev/vcslocator@cb1adf0eb1179e26228091c3a347d037ae7b4460#components.go",
+				"git+https://github.com/carabiner-dev/actions@ecdd8b03b5c1bad78d5d89ab71e1ca9bb5ad31c9#drop/action.yml",
+				"git+https://github.com/carabiner-dev/actions@ecdd8b03b5c1bad78d5d89ab71e1ca9bb5ad31c9#install/ampel/action.yml",
+				"git+https://github.com/carabiner-dev/actions@ecdd8b03b5c1bad78d5d89ab71e1ca9bb5ad31c9#install/bnd/action.yml",
+				"git+https://github.com/carabiner-dev/actions@3a2820538c0bfe5be1ad7589a68d03823f403c5c#install/ampel/action.yml",
+			},
+			[]string{
+				"58c76f62c2d403aa2d946f53b381f4948f12a6814482d50fb4fd3d87f45e38d3",
+				"58c76f62c2d403aa2d946f53b381f4948f12a6814482d50fb4fd3d87f45e38d3",
+				"abf988eca60b353c1a1a030219466acc8d355c35a1e40c508e392dd3496be525",
+				"e8d84a48c5240adeb41ba5d66fce91cc6df2ef87031debacdd8ab56f40c2227b",
+				"17635be05f865e1efeeaba6c83db9c80bfdd09be56c4fe8504eacc55cfd3fd88",
+				"e8d84a48c5240adeb41ba5d66fce91cc6df2ef87031debacdd8ab56f40c2227b",
+			},
+			false,
+		},
+		{
+			"one-errs",
+			[]string{"git+https://github.com/carabiner-dev/vcslocator@76241a877eb3374f6017224c61d6a167c337de4d#.gitignore2"},
+			[]string{},
+			true,
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			dataGroup, err := GetGroup(tc.locators)
+			if tc.mustErr {
+				require.Error(t, err)
+				return
+			}
+			require.NoError(t, err)
+			require.Len(t, dataGroup, len(tc.locators))
+
+			for i, data := range dataGroup {
+				h := sha256.New()
+				h.Write(data)
+				require.Equal(t, tc.expect[i], fmt.Sprintf("%x", h.Sum(nil)))
+			}
 		})
 	}
 }
